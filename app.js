3b45686a073a43db98d8010534eec730ed9ab514
quangtruong452001 <58112251+quangtruong452001@users.noreply.github.com>
2021-12-03 17:52:30 +0700

    Remove mongoose-encrytion, change to md5 for hashing pass

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encryp = require("mongoose-encryption");
+// const encryp = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -24,8 +25,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
-userSchema.plugin(encryp, {secret: process.env.SECRET, encryptedFields: ["password"]});
+//Encryp using mongoose-encryp
+// userSchema.plugin(encryp, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,7 +46,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -60,7 +61,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if(err){
